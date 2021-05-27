0a19b8187f7dba151420d050d6ad8946f73ecb9f
GABRIEL PORTO <GR_PORTO@HOTMAIL.COM>
2021-05-26 22:26:41 -0300

    Pedido - Command Handler - Parte 1

--- 02 - TDD/src/NerdStore.Core/Messages/Message.cs
+++ 02 - TDD/src/NerdStore.Core/Messages/Message.cs
@@ -1,9 +1,11 @@
-﻿namespace NerdStore.Core.Messages
+﻿using System;
+
+namespace NerdStore.Core.Messages
 {
     public abstract class Message
     {
         public string MessageType { get; protected set; }
-        public string AggregateId { get; protected set; }
+        public Guid AggregateId { get; protected set; }
 
         protected Message()
         {
